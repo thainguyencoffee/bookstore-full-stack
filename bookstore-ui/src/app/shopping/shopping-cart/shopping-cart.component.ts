64e11738ee135e81ded773b4780491c68e659e6a
thainguyencoffee <nguyennt11032004@gmail.com>
2024-07-09 08:14:06 +0700

    load book-details when enter the shopping-cart component (forkjoin, takeUtil)

--- bookstore-ui/src/app/shopping/shopping-cart/shopping-cart.component.ts
+++ bookstore-ui/src/app/shopping/shopping-cart/shopping-cart.component.ts
@@ -1,17 +1,13 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {ShoppingCartService} from "../../shared/service/shopping-cart.service";
 import {CommonModule} from "@angular/common";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
-import {distinctUntilChanged, map, Observable} from "rxjs";
+import {forkJoin, Subject, switchMap, takeUntil} from "rxjs";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 import {BookQuantityComponent} from "../../shared/component/book-quantity/book-quantity.component";
 import {RouterModule} from "@angular/router";
 import {BookService} from "../../shared/service/book.service";
-import {Book} from "../../shared/model/book";
-import {SnackbarService} from "../../shared/service/snackbar.service";
-import {CustomError} from "../../shared/model/api-error";
 import {BookCardComponent} from "../../shared/component/book-card/book-card.component";
 import {ShoppingCart} from "../../shared/model/shopping-cart";
 
@@ -31,13 +27,36 @@ import {ShoppingCart} from "../../shared/model/shopping-cart";
   templateUrl: './shopping-cart.component.html',
   styleUrl: './shopping-cart.component.css'
 })
-export class ShoppingCartComponent implements OnInit{
+export class ShoppingCartComponent implements OnInit, OnDestroy {
   private shoppingCartService = inject(ShoppingCartService);
+  private bookService = inject(BookService);
   shoppingCart: ShoppingCart | undefined;
+  private destroy$ = new Subject<void>();
 
   ngOnInit(): void {
     this.shoppingCartService.$shoppingCart
-      .subscribe(cart => this.shoppingCart = cart)
+      .pipe(
+        switchMap(cart => {
+          this.shoppingCart = cart;
+          const bookDetailObservables = cart!.cartItems.map(item =>
+            this.bookService.getBookByIsbn(item.isbn)
+          );
+          return forkJoin(bookDetailObservables);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(bookDetails => {
+        if (this.shoppingCart) {
+          this.shoppingCart.cartItems.forEach((item, index) => {
+            item.bookDetail = bookDetails[index];
+          });
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
