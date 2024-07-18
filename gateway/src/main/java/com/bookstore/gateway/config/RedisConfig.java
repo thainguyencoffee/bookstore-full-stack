29ae0d7e8e5176e27e4e41cc6ea484a8fc6be4b9
thainguyencoffee <nguyennt11032004@gmail.com>
2024-07-18 15:58:27 +0700

    RedisConfig fix

--- gateway/src/main/java/com/bookstore/gateway/config/RedisConfig.java
+++ gateway/src/main/java/com/bookstore/gateway/config/RedisConfig.java
@@ -3,12 +3,14 @@ package com.bookstore.gateway.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@Profile("prod")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
