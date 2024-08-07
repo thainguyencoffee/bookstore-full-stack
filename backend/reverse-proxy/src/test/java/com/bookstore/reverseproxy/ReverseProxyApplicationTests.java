7a471889ef6fb39ea1a2470f2e4c0a470e23d1bd
thainguyencoffee <nguyennt11032004@gmail.com>
2024-08-08 03:48:49 +0700

    add bff (cloud gateway), reverse-proxy (cloud-gateway), init vue

---
+++ backend/reverse-proxy/src/test/java/com/bookstore/reverseproxy/ReverseProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.reverseproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
