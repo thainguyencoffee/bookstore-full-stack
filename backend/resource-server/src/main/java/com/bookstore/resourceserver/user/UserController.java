6e2a80cc0cee9e6ac99ebcb7faf7d6e8c30f7098
thainguyencoffee <nguyennt11032004@gmail.com>
2024-08-09 22:19:26 +0700

    add

--- backend/resource-server/src/main/java/com/bookstore/resourceserver/user/UserController.java
+++ backend/resource-server/src/main/java/com/bookstore/resourceserver/user/UserController.java
@@ -1,15 +1,13 @@
 package com.bookstore.resourceserver.user;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.util.Date;
 import java.util.Optional;
 
 @RestController
@@ -18,26 +16,13 @@ public class UserController {
     @GetMapping("/api/me")
     public UserInfoDto getMe(Authentication auth) {
         if (auth instanceof JwtAuthenticationToken jwtAuth) {
-            final var email = (String) jwtAuth.getTokenAttributes()
-                    .getOrDefault(StandardClaimNames.EMAIL, "");
-            final var roles = auth.getAuthorities()
-                    .stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .toList();
-            final var exp = Optional.ofNullable(jwtAuth.getTokenAttributes().get(JwtClaimNames.EXP))
-                    .map(expClaim -> {
-                        if (expClaim instanceof Long lexp) {
-                            return lexp;
-                        }
-                        if (expClaim instanceof Instant iexp) {
-                            return iexp.getEpochSecond();
-                        }
-                        if (expClaim instanceof Date dexp) {
-                            return dexp.toInstant().getEpochSecond();
-                        }
-                        return Long.MAX_VALUE;
-                    }).orElse(Long.MAX_VALUE);
-            return new UserInfoDto(auth.getName(), email, roles, exp);
+            Jwt jwt = jwtAuth.getToken();
+            final var username = jwt.getClaimAsString(StandardClaimNames.PREFERRED_USERNAME);
+            final var email = Optional.ofNullable(jwt.getClaimAsString(StandardClaimNames.EMAIL)).orElse("");
+            final var roles = jwt.getClaimAsStringList("roles");
+            final var exp = jwt.getClaimAsInstant(JwtClaimNames.EXP);
+
+            return new UserInfoDto(username, email, roles, exp);
         }
         return UserInfoDto.ANONYMOUS;
     }
