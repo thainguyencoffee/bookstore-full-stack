71c757861ba8661eb05fa382baa33260ccacd9f5
thainguyencoffee <nguyennt11032004@gmail.com>
2024-08-13 15:43:17 +0700

    missing

--- backend/resource-server/src/main/java/com/bookstore/resourceserver/purchaseorder/LineItem.java
+++ backend/resource-server/src/main/java/com/bookstore/resourceserver/purchaseorder/LineItem.java
@@ -1,10 +1,14 @@
 package com.bookstore.resourceserver.purchaseorder;
 
+import com.bookstore.resourceserver.core.valuetype.Price;
+import com.bookstore.resourceserver.purchaseorder.valuetype.BookType;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.AccessType;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Embedded;
 import org.springframework.data.relational.core.mapping.Table;
 
 import jakarta.validation.constraints.Min;
@@ -21,6 +25,7 @@ public class LineItem {
     @NotNull(message = "Book is required")
     private UUID orderId;
     private String isbn;
+    private BookType bookType;
     @NotNull(message = "Quantity is required")
     @Min(value = 1, message = "Quantity must be greater than 0")
     private Integer quantity;
@@ -30,7 +35,14 @@ public class LineItem {
     private int version;
 
     @AccessType(AccessType.Type.PROPERTY)
-    public Long getTotalPrice() {
-        return price * quantity;
+    @Transient
+    public Price getTotalPrice() {
+        Price total = new Price();
+        if (price.getDiscountedPrice() != null) {
+            total.setDiscountedPrice(price.getDiscountedPrice() * quantity);
+        }
+        total.setOriginalPrice(price.getOriginalPrice() * quantity);
+        return total;
     }
+
 }
